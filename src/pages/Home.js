dcda905853eca71de0986b21a838c7c7d5db50c7
Vera Testard <vera.testard@gmail.com>
2019-04-10 11:03:42 +0200

    Premier cours

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import WelcomeCard from "../components/WelcomeCard";
+
+class Home extends Component {
+  render() {
+    return <WelcomeCard />;
+  }
+}
+
+export default Home;
